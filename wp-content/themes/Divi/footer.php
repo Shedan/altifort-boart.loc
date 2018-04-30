a117a6115871b91047673cbc86c9037aab5397ff
shedan <shedanua@gmail.com>
2018-04-30 19:34:59 +0300

    Initial commit

---
+++ wp-content/themes/Divi/footer.php
@@ -0,0 +1,53 @@
+<?php if ( 'on' == et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
+
+	<span class="et_pb_scroll_top et-pb-icon"></span>
+
+<?php endif;
+
+if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+
+			<footer id="main-footer">
+				<?php get_sidebar( 'footer' ); ?>
+
+
+		<?php
+			if ( has_nav_menu( 'footer-menu' ) ) : ?>
+
+				<div id="et-footer-nav">
+					<div class="container">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer-menu',
+								'depth'          => '1',
+								'menu_class'     => 'bottom-nav',
+								'container'      => '',
+								'fallback_cb'    => '',
+							) );
+						?>
+					</div>
+				</div> <!-- #et-footer-nav -->
+
+			<?php endif; ?>
+
+				<div id="footer-bottom">
+					<div class="container clearfix">
+						<div class="et-social-icons">
+						<a href="http://altifort.com/"	style="
+    font-size: 24px;
+    font-weight: 600;
+    color: #fff;
+    letter-spacing: 10px;
+">ALTIFORT</a>
+						</div>
+					</div>	<!-- .container -->
+				</div>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+
+	</div> <!-- #page-container -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
